79c225e23e8bb5a206d0bcde58b879c79f703be9
josh <josh91425@gmail.com>
2022-11-12 01:36:21 +0800

    Scene and script for change scene

---
+++ Assets/Script/ControlScore.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
+
+
+//using static ScoreManager;
+public class ControlScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private float score;
+    
+    
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        int xCenter = (Screen.width / 2);
+        int yCenter = (Screen.height / 2);
+        int width = 400;
+        int height = 120;
+
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("button"));
+        fontSize.fontSize = 32;
+
+        Scene scene = SceneManager.GetActiveScene();
+        
+        if (scene.name == "Scene1")
+        {
+            //Show a button to allow scene2 to be switched to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Load second scene", fontSize))
+            {
+                ScoreManager.Instance.IncreaseScore(25.0f); 
+                SceneManager.LoadScene("Scene2");
+                //animator.SetTrigger("FadeOut");
+            }
+        }
+        else if(scene.name == "Scene2")
+        {
+            // Show a button to allow scene1 to be returned to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Return to first scene", fontSize))
+            {
+                ScoreManager.Instance.IncreaseScore(25.0f); 
+                animator.SetTrigger("FadeOut");
+                
+                
+            }
+        }
+        
+    }
+    public void LoadtoNextScene() {
+        SceneManager.LoadScene("Scene1");
+    }
+}
---
+++ Assets/Script/DontDestroy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class DontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Score");
+
+        if (objs.Length > 1)
+        {
+            
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private TextMeshProUGUI MyScoreText;
+    //private int score = 0;
+    void Start()
+    {
+        MyScoreText = FindObjectOfType<TextMeshProUGUI>();
+        //print(MyScoreText);
+        MyScoreText.SetText("Score: " + ScoreManager.Instance.Score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyScoreText = FindObjectOfType<TextMeshProUGUI>();
+        //print(MyScoreText);
+        MyScoreText.SetText("Score: " + ScoreManager.Instance.Score);
+    }
+}
+
