b5a40dc712487bb1b87fc7baf62d38caf4cc8038
squaremelon <iamjasper0828@gmail.com>
2022-11-16 03:16:04 +0800

    add lose scene, effect, and clean up codes

--- Assets/Script/BulletCreate.cs
+++ Assets/Script/BulletCreate.cs
@@ -11,15 +11,12 @@ public class BulletCreate : MonoBehaviour
     public GameObject ExplodeEffect;
 
     private GameController gameController;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         Destroy(gameObject, ExistTime);
         gameController = GameControllerObj.GetComponent<GameController>();
     }
-
-    // Update is called once per frame
     
     void Update()
     {
@@ -33,14 +30,13 @@ public class BulletCreate : MonoBehaviour
             if (hit.collider.CompareTag("Enemy")) {
                 gameController._PlayerBulletHitOn(hit.collider.gameObject);
                 Instantiate(ExplodeEffect, hit.point, Quaternion.identity);
-                //print("yes" + i);
                 Debug.Log(hit.collider.name);
                 takedamage(hit.transform);
-                //i++;
                 Destroy(gameObject);
             }
         }
     }
+
     void takedamage(Transform enemy) {
         Scene2Enemy e1 = enemy.GetComponent<Scene2Enemy>();
         enemyScript e2 = enemy.GetComponent<enemyScript>();
@@ -55,6 +51,7 @@ public class BulletCreate : MonoBehaviour
         else if (ee)
             ee.Damage();
     }
+
     void OnCollisionEnter(Collision other) {
         if (!other.collider.CompareTag("Player")) {
             if (other.collider.CompareTag("Enemy")) {
@@ -63,8 +60,5 @@ public class BulletCreate : MonoBehaviour
             }
             Destroy(gameObject);
         }
-        // if(other.gameObject == "Enemy") {
-        //     takedamage(other.transform);
-        // }
     }
 }
