4ae27af53c95638796cf4ee09720545cc84c0e7e
dinoslow <73374146+dinoslow@users.noreply.github.com>
2022-11-16 03:01:49 +0800

    merge audio

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -12,7 +12,7 @@ public class SceneTransition : MonoBehaviour
     public Wizard W3;
     public Wizard W4;
     public Wizard W5;
-    int flag = 0;
+    // int flag = 0;
     void Start()
     {
         
@@ -21,17 +21,6 @@ public class SceneTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // print("W1 hp: "+ W1.GetHp());
-        // print("W2 hp: "+ W2.GetHp());
-        // print("W3 hp: "+ W3.GetHp());
-        // print("W4 hp: "+ W4.GetHp());
-        // print("W5 hp: "+ W5.GetHp());
-        // if(flag == 0) {
-        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-        //     flag = 1;
-        // }
-            
-
         if(W1.GetHp() <= 0 && W2.GetHp() <= 0 && W3.GetHp() <= 0 && W4.GetHp() <= 0 && W5.GetHp() <= 0) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         }
