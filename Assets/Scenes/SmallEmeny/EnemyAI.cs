db59a9ab08a933fbb976895ea0300d644cdbd20b
josh <josh91425@gmail.com>
2022-11-30 19:33:15 +0800

    spider

---
+++ Assets/Scenes/SmallEmeny/EnemyAI.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private UnityEngine.AI.NavMeshAgent agent;
+    private Transform player;
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    public AudioSource audiosource;
+    public AudioClip aclip;
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    //public GameObject projectile;
+    public float health;
+    //States
+    public float sightRange = 10.0f;
+    public float attackRange = 5.0f;
+    public bool playerInSightRange, playerInAttackRange;
+    public LayerMask whatground;
+    public Animator animator;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Start() {
+        animator.SetTrigger("Roar");
+        audiosource.PlayOneShot(aclip);
+    }
+    void Death() {
+        if(health <= 0.0f) {
+            animator.SetTrigger("Die");
+        }
+    }
+    private void Update()
+    {
+        //Check for sight and attack range
+        float dis = Vector3.Distance(player.position,transform.position);
+       
+        if(dis < sightRange) {
+            playerInSightRange = true;
+        } else {
+            playerInSightRange = false;
+        }
+        if(dis < attackRange) {
+            playerInAttackRange = true;
+        } else {
+            playerInAttackRange = false;
+        }
+
+        if (!playerInSightRange && !playerInAttackRange) {
+            animator.SetBool("Slither Fast Forward",true);
+            print(1);
+            Patroling();
+        }
+        if (playerInSightRange && !playerInAttackRange) {
+            print(2);
+            ChasePlayer();
+        }
+        if (playerInAttackRange && playerInSightRange) {
+            //animator.SetBool("Slither Fast Forward",false);
+            print(3);
+            AttackPlayer();
+        }
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        // if (Physics.Raycast(walkPoint, -transform.up, 2f,whatground))
+        // if(walkPoint.x < 30.0f && walkPoint.x > -25.0f && walkpoint)
+        //transform.LookAt(walkPoint);
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+            // Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            // rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            // rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
+            animator.SetTrigger("Spit Attack");
+            //animator.SetTrigger("Cast Spell");
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    // public void TakeDamage(int damage)
+    // {
+    //     health -= damage;
+
+    //     if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    // }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
+
+
---
+++ Assets/Scenes/SmallEmeny/SpiderSilk.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderSilk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public GameObject smallspider;
+    // public GameObject Boss;
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     Vector3 Face = player.transform.position - Boss.transform.position;
+    //     Ray ray = new Ray(transform.position,Face);
+    //     if (Physics.Raycast(ray, out hit)) {
+    //         if (hit.collider.CompareTag("Player")) {
+    //             smallspider.GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(player.transform.position);
+    //         }
+    //     }
+    // }
+}
