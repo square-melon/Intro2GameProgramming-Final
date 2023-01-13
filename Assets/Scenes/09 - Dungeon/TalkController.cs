449741536d5609a8283540cf551ab28b4cc82212
dinoslow <73374146+dinoslow@users.noreply.github.com>
2023-01-14 07:04:35 +0800

    finish rookie

---
+++ Assets/Scenes/09 - Dungeon/TalkController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkController : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Mentor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
