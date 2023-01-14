73e0247d63ab87018f95f87a4c6e5ba4d49d59dd
minfan-Yu <minfan0510@gmail.com>
2023-01-14 08:05:47 +0800

    minimap

---
+++ Assets/minimapScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimapScript : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        //transform.rotation = Quaternion.Euler(90f,player.eulerAngles.y,0f);
+    }
+}
